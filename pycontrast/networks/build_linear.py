36b867a799663d6df41a66548ab95d5f165d6d40
Jiahao Liu <liujiahao11@outlook.com>
2022-01-15 17:34:40 +0800

    fix

--- pycontrast/networks/build_linear.py
+++ pycontrast/networks/build_linear.py
@@ -10,13 +10,9 @@ def build_linear(opt):
         n_feat = 2048 * 2
     else:
         n_feat = 2048
-    classifier = nn.Sequential(nn.Linear(n_feat,int(n_feat//2)),
-                 nn.Linear(int(n_feat//2),int(n_feat//2**2)),
-                 nn.Linear(int(n_feat//2**2),int(n_feat//2**3)),
-                 nn.Linear(int(n_feat//2**3),int(n_feat//2**4)),
-                 nn.Linear(int(n_feat//2**4),int(n_feat//2**5)),
-                 nn.Linear(int(n_feat//2**5),int(n_feat//2**6)),
-                 nn.Linear(int(n_feat//2**6),int(n_feat//2**7)),
-                 nn.Linear(int(n_feat//2**7),n_class),)
-    classifier = nn.Linear(n_feat, n_class)
+    classifier = nn.Sequential(nn.Linear(n_feat,int(n_feat//2**2)),nn.ReLU(inplace=True),
+                 nn.Linear(int(n_feat//2**2),int(n_feat//2**4)),nn.ReLU(inplace=True),
+                 nn.Linear(int(n_feat//2**4),int(n_feat//2**6)),nn.ReLU(inplace=True),
+                 nn.Linear(int(n_feat//2**6),n_class),)
+    # classifier = nn.Linear(n_feat, n_class)
     return classifier
